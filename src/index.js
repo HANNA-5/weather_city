8b0d28670555c744f3c6523a5fd0d24f181936b6
HANNA-5 <al.ivakh77@gmail.com>
2022-10-09 12:20:24 +0200

    icon

--- src/index.js
+++ src/index.js
@@ -20,13 +20,31 @@ function formatDay(){
 }
 formatDay();
 
+function weather(response){
+  //2 вар-т  //  document.querySelector("#humidityNow").innerHTML=`${response.data.main.humidity} %`;
+
+     //let temperatureNow =Math.round(response.data.main.temp);
+     document.querySelector("h3").innerHTML=`${Math.round(response.data.main.temp)}℃`;
+    
+      let humidityElement= document.querySelector("#humidityNow");
+     let windElement= document.querySelector("#windNow");
+     let iconElement=document.querySelector("#icon");
+      humidityElement.innerHTML=`HUMIDITY:${response.data.main.humidity} %`;
+      
+     windElement.innerHTML=`WIND:${Math.round(response.data.wind.speed)} km/h`;
+     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+     iconElement.setAttribute("alt",response.data.weather[0].description); 
+     }
+
+
 function showWeather(event){
-    event.preventDefaul();
+    event.preventDefault();
     let cityIn= document.querySelector("#inputCity").value;
-    document.querySelector("#cityType").innerHTML=cityIn.toUpperCase();
-    let apiKey = "8161b4309ee03faae957729ba7104797";
+    document.querySelector("h2").innerHTML=cityIn.toUpperCase();
+    let apiKey = "bc2cd97eaa209e7d22d8f3c84081655f";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityIn}&appid=${apiKey}&&units=metric`;
     axios.get(url).then(weather);
+   
 }
 let cityForm=document.querySelector("#city-form");
 cityForm.addEventListener("submit",showWeather);
