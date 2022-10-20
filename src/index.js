d7bc2f9e52c57eb0be3500a48f23b978e847048f
HANNA-5 <al.ivakh77@gmail.com>
2022-10-20 17:55:50 +0200

    forecast

--- src/index.js
+++ src/index.js
@@ -54,25 +54,27 @@ cityForm.addEventListener("submit",showWeather);
 
 
 
-//function displayForecast(response){
-    //let forecast=response.data.daily;
-   // let forecastHTML=`<div class="row">`;
-    //forecast.forEach.function(forecastDay,index); { 
-      //  if(index<6){forecastHTML=forecastHTML+
-      //  `<div class="col-2"><div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-   // }
-//<img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"alt="" width="42"/>   
-//<div class="weatherForecast" >
-//<span class="weatherForecast-temperature-max">${Math.round(forecastDay.temp.max)}℃ </span>
-//<span class="weatherForecast-temperature-min">${Math.round(forecastDay.temp.min)}℃ </span>
-//</div>
-//</div>
-//`
-//;
-//}
-//let apiKey = "bc2cd97eaa209e7d22d8f3c84081655f";
-//let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityIn}&appid=${apiKey}&&units=metric`;
-//axios.get(url).then(displayForecast);
-//} ;
- //forecastHTML=forecastHTML+`</div>`;
- //forecastElement.innerHTML=forecastHTML;
+function displayForecast(response) {
+    let forecast=response.data.daily ;
+   let forecastHTML=`<div class="row">`;
+    forecast.forEach.function(forecastDay,index); { 
+       if(index < 6){forecastHTML=forecastHTML+
+       `<div class="col-2"><div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+   }
+<img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"alt="" width="42"/>   
+<div class="weatherForecast" >
+<span class="weatherForecast-temperature-max">${Math.round(forecastDay.temp.max)}℃ </span>
+<span class="weatherForecast-temperature-min">${Math.round(forecastDay.temp.min)}℃ </span>
+</div>
+</div>
+`
+;
+}
+let apiKey = "bc2cd97eaa209e7d22d8f3c84081655f";
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityIn}&appid=${apiKey}&&units=metric`;
+axios.get(url).then(displayForecast);
+} ;
+ forecastHTML=forecastHTML+`</div>`;
+
+ let forecastElement = document.getElementByID('#days');
+ forecastElement.innerHTML=forecastHTML;
